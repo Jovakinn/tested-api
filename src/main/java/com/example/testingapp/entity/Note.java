45f652afeee60402cded7817a0922fd18344e964
Jovakinn <max05012004@gmail.com>
2022-08-04 20:36:05 +0300

    add new entities

---
+++ src/main/java/com/example/testingapp/entity/Note.java
@@ -0,0 +1,29 @@
+package com.example.testingapp.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "notes")
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Note {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "person_id")
+    private Person person;
+
+    public Note(String body) {
+        this.body = body;
+    }
+}
---
+++ src/main/java/com/example/testingapp/entity/Person.java
@@ -0,0 +1,38 @@
+package com.example.testingapp.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "persons")
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.PERSIST)
+    @ToString.Exclude
+    private List<Note> notes = new ArrayList<>();
+
+    public void addNote(Note note) {
+        note.setPerson(this);
+        notes.add(note);
+    }
+}
